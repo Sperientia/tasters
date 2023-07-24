3a04839d17892b575475103359c1f1383b2cb1e5
BubuDavid <david.pedroza.segoviano@gmail.com>
2023-07-23 20:11:25 -0600

    🍪 [REFACTOR] Refactor v2.0🍪

--- src/data/header.js
+++
@@ -1,7 +0,0 @@
-export const headerFields = [
-	{ label: 'Nosotros', href: '/' },
-	{ label: 'Reclutamiento', href: '/' },
-	{ label: 'Blog', href: '/' },
-	{ label: 'Contacto', href: '/' },
-	{ label: 'Registro', href: '/', type: 'button' },
-]
